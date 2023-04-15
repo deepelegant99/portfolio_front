96df8340ada5f8dde15fded009b1ed86d0605a7a
phillip wong <deepelegant99@gmail.com>
2023-04-14 21:14:38 -0700

    comment out non-relevant code

--- src/pages/SingleProject.jsx
+++ src/pages/SingleProject.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import { useParams } from "react-router-dom";
 const SingleProject = () => {
-  
-  return (
-    <div>SingleProject</div>
-  )
-}
+  // const { projectId } = useParams();
+  // console.log(projectId)
+  return <div>SingleProject</div>;
+};
 
-export default SingleProject
+export default SingleProject;
